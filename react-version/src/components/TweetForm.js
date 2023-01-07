95411414c1c8fff436dc054ee59264594f0b1613
Wesley Matthee <wrmatthee@gmail.com>
2023-01-07 08:48:25 -0500

    React version of tweeter day 01

---
+++ react-version/src/components/TweetForm.js
@@ -0,0 +1,18 @@
+import React from 'react'; //optional
+
+function TweetForm() {
+  const textHere = "What are you humming about?";
+  
+  return (
+      <section className="newtweet">
+        <form method="post" action="/tweets" className="newtweet__form" onSubmit={(event) => event.preventDefault()} >
+          <textarea className="form__textarea" name="text" placeholder={textHere}></textarea>
+        <input type="submit" value="Tweet" className="form__input" onClick={(event) => event.preventDefault()} />
+          <span className="form__counter">140</span>
+        </form>
+      </section>
+   
+  );
+}
+
+export default TweetForm;
